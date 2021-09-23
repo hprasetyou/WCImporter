0b120e35c8c4012b4b53c33077532bcce07dcbe3
Patrick Star <hprasetyou@gmail.com>
2021-09-23 11:15:04 +0700

    add config

--- Helper/WPFetch.php
+++ Helper/WPFetch.php
@@ -7,9 +7,8 @@ class WPFetch extends AbstractHelper{
         \Magento\Framework\App\Helper\Context $context)
   {
       parent::__construct($context);
-      $this->consumerKey = 'ck_72fd48880aa159ce3e7d7418dfb1c8fa3262acac';
-      $this->consumerSecret = 'cs_67a31154c926192e56a24f94d03f543617c5bc9c';
-
+      $this->consumerKey = $this->scopeConfig->getValue('hpu_wci/general/consumer_key', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+      $this->consumerSecret =  $this->scopeConfig->getValue('hpu_wci/general/consumer_secret', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
       $this->oauth = new \OAuth($this->consumerKey, $this->consumerSecret);
       $this->curl = curl_init();
   }
